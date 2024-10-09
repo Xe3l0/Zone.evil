6179dfe6358e931bf7d94a421641a4bf871e6f00
Xe3l0 <168476962+Xe3l0@users.noreply.github.com>
2024-10-09 03:47:48 +0100

    Add files via upload

---
+++ شروحات.js
@@ -0,0 +1,41 @@
+replace=وضع مسافه
+/\$/g," "=ايضا مسافه
+<h1> </h1> نص كبير 
+<head></head> ربط لغات و تغير اسم موقع
+<body></body> اي شئ ينكتب هنا ينطبعةفي موقع 
+<i></i> كلمه مائلة
+<p></p> كتب نص
+<b></b> تكبير خط نص
+<font></font> تغير خط و شكله
+color="--" لون
+size="--" حجم خط
+face="--" ستايل خط
+allgn="--"(center - منتصف) + (raght - يمين) = تغير مكان نص 
+
+<body align="right" dir="ltr">
+ <p>  اسمي omar byrok </p>
+</body>
+
+</html> = طريقه تعديل لغه العربيه
+table= = جدول
+<tr><td>1<td><tr> >1234 = عدد خنات جدول
+ <th colspan="5">مترشحين</th> = صف افوقي
+ bgolor="" = لون جدول
+ <a href="--">--</a> = رابط 
+ <img src="---" alt="" width=""> = اضافه صوره للموقع
+ <video src="---" width="400" controls poster=""></video> = اضافه فيديو
+ <ol>
+ <li></li>
+ </ol> = قائمه
+ 
+ <nav>
+    <div class="logo">cayton</div>
+    <ul class="nav-links">
+  <li><a href="1.html">خدمه عملاء</a></li>
+  <li><a href="2.html">حساباتي</a></li>
+  <li><a href="3.html">قائمه</a></li>
+</ul>
+  </nav> شريط
+ 
+ ------------------- css -------------------------
+ font-size
